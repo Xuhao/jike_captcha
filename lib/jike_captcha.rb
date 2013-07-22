2bd70deb1f2c1c531a174b31fc89fca80bc866f2
Xuhao <xuhao@rubyfans.com>
2013-07-22 22:55:59 +0800

    MISC - First commit

---
+++ lib/jike_captcha.rb
@@ -0,0 +1,7 @@
+require "jike_captcha/version"
+
+module JikeApi
+  module Captcha
+    # Your code goes here...
+  end
+end
---
+++ lib/jike_captcha/version.rb
@@ -0,0 +1,5 @@
+module JikeApi
+  module Captcha
+    VERSION = "0.0.1"
+  end
+end
