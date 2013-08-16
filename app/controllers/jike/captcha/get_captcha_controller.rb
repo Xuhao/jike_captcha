3642efea33b388da56a6f1ec1141a3984266e64e
Xuhao <xuhao@rubyfans.com>
2013-08-16 17:10:51 +0800

    MISC - DRY GetCaptchaController

--- app/controllers/jike/captcha/get_captcha_controller.rb
+++ app/controllers/jike/captcha/get_captcha_controller.rb
@@ -2,9 +2,10 @@ module Jike::Captcha
   class GetCaptchaController < ::ActionController::Metal
 
     def index
-      Jike::Captcha::Helpers.reset_captcha!
-      image = (params[:image_type] == 'data_url' ? Jike::Captcha::Helpers.captcha_image_data_url : Jike::Captcha::Helpers.captcha_image_url)
-      self.response_body = {captcha_image: image, captcha_id: Jike::Captcha::Helpers.captcha_id}.to_json
+      jike_helper = Jike::Captcha::Helpers
+      jike_helper.reset_captcha!
+      image = (params[:image_type] == 'data_url' ? jike_helper.captcha_image_data_url : jike_helper.captcha_image_url)
+      self.response_body = { captcha_image: image, captcha_id: jike_helper.captcha_id }.to_json
     end
   end
 end
